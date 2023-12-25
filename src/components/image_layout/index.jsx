ac70115a528f707f3da32ce1153d4548dab0bace
karthik <karthik5112001@gmail.com>
2023-12-25 15:18:24 +0530

    center text added

--- src/components/image_layout/index.jsx
+++ src/components/image_layout/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+
+import { AOS_ANIMATION } from "../../constant";
+
 import styles from "./image.module.css";
 
 const ImageLayout = ({ image, className }) => {
   return (
-    <div className={`${styles.container} ${className}`}>
+    <div
+      className={`${styles.container} ${className}`}
+      data-aos={AOS_ANIMATION.FADE_UP.TYPE}
+      data-aos-duration={AOS_ANIMATION.FADE_UP.DURATION}
+    >
       <div className={styles.corner1}></div>
       <div className={styles.corner2}></div>
       <div className={styles.corner3}></div>
